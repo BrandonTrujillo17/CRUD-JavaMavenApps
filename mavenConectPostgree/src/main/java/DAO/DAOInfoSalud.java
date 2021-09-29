99eeb63416673b4c79dba546af6a98dedbe63db2
Josue Capistran García <josuecapistran@gmail.com>
2021-09-29 17:32:18 -0500

    Funcionalida de datos medicos añadida y completada

--- mavenConectPostgree/src/main/java/DAO/DAOInfoSalud.java
+++ mavenConectPostgree/src/main/java/DAO/DAOInfoSalud.java
@@ -27,19 +27,73 @@ public class DAOInfoSalud {
             consulta.setString(1, matriculaEstudiante);
             ResultSet rs =  consulta.executeQuery();
             while(rs.next()){
-                infoEstudiante.setIdInfo(rs.getInt(1));
+                infoEstudiante.setIdInfo(rs.getInt(5));
                 infoEstudiante.setNumeroSeguro(rs.getString(2));
-                infoEstudiante.setTipoSangre(rs.getString(3));
-                infoEstudiante.setTipoSeguro(rs.getString(4));
+                infoEstudiante.setTipoSangre(rs.getString(4));
+                infoEstudiante.setTipoSeguro(rs.getString(3));
+                infoEstudiante.setEstatura(rs.getString(6));
+                infoEstudiante.setPeso(rs.getString(7));
+                infoEstudiante.setDiscVisual(rs.getString(8));
+                infoEstudiante.setDiabetes(rs.getString(9));
+                infoEstudiante.setHipertención(rs.getString(10));
             }
             conn.close();
             consulta.close();
             rs.close();
         }catch(SQLException ex){
-            System.out.println("Error: " + ex.getMessage());
+            ex.printStackTrace();
             JOptionPane.showMessageDialog(null, "Error al conectar con la BD");
         }
         
     return infoEstudiante;
+    }
+    
+    public int registrarInfMedica(InfoSalud infoSalud, String matricula){
+        int resultado = 0;
+        try{
+            Connection conn = Conexion.Conectar();
+            PreparedStatement consulta = conn.prepareStatement("INSERT INTO inf_salud (estudiante_idestudiante, numero_seguro, tipo_seguro, grupo_sanguineo, "
+                    + "estatura, peso, discapacidadvisual, diabetes, hipertencion) "
+                    + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);");
+            consulta.setString(1, matricula);
+            consulta.setString(2, infoSalud.getNumeroSeguro());
+            consulta.setString(3, infoSalud.getTipoSeguro());
+            consulta.setString(4, infoSalud.getTipoSangre());
+            consulta.setString(5, infoSalud.getEstatura());
+            consulta.setString(6, infoSalud.getPeso());
+            consulta.setString(7, infoSalud.getDiscVisual());
+            consulta.setString(8, infoSalud.getDiabetes());
+            consulta.setString(9, infoSalud.getHipertención());
+            resultado = consulta.executeUpdate();
+            conn.close();
+            consulta.close();
+        }catch(SQLException ex){
+            ex.printStackTrace();
+        } 
+        return resultado;
+    }
+    
+    public int actualizarInfMedica(InfoSalud infoSalud, String matricula){
+        int resultado = 0;
+        try{
+            Connection conn = Conexion.Conectar();
+            PreparedStatement consulta = conn.prepareStatement("UPDATE inf_salud SET numero_seguro=?, tipo_seguro=?, grupo_sanguineo=?, "
+                    + "estatura=?, peso=?, discapacidadvisual=?, diabetes=?, hipertencion=? WHERE estudiante_idEstudiante=?;");
+            consulta.setString(9, matricula);
+            consulta.setString(1, infoSalud.getNumeroSeguro());
+            consulta.setString(2, infoSalud.getTipoSeguro());
+            consulta.setString(3, infoSalud.getTipoSangre());
+            consulta.setString(4, infoSalud.getEstatura());
+            consulta.setString(5, infoSalud.getPeso());
+            consulta.setString(6, infoSalud.getDiscVisual());
+            consulta.setString(7, infoSalud.getDiabetes());
+            consulta.setString(8, infoSalud.getHipertención());
+            resultado = consulta.executeUpdate();
+            conn.close();
+            consulta.close();
+        }catch(SQLException ex){
+            ex.printStackTrace();
+        } 
+        return resultado;
     }
 }
--- mavenConectPostgree/src/main/java/pocos/InfoSalud.java
+++ mavenConectPostgree/src/main/java/pocos/InfoSalud.java
@@ -14,16 +14,69 @@ public class InfoSalud {
     private String tipoSeguro;
     private String tipoSangre;
     private int idInfo;
+    private String estatura;
+    private String peso;
+    private String discVisual;
+    private String diabetes;
+    private String hipertención;
+    
 
     public InfoSalud() {
     }
-    
-    public InfoSalud(String numeroSeguro, String tipoSeguro, String tipoSangre, int idInfo) {
+
+    public InfoSalud(String numeroSeguro, String tipoSeguro, String tipoSangre, int idInfo, String estatura, String peso, String discVisual, String diabetes, String hipertención) {
         this.numeroSeguro = numeroSeguro;
         this.tipoSeguro = tipoSeguro;
         this.tipoSangre = tipoSangre;
         this.idInfo = idInfo;
+        this.estatura = estatura;
+        this.peso = peso;
+        this.discVisual = discVisual;
+        this.diabetes = diabetes;
+        this.hipertención = hipertención;
     }
+
+    public String getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(String estatura) {
+        this.estatura = estatura;
+    }
+
+    public String getPeso() {
+        return peso;
+    }
+
+    public void setPeso(String peso) {
+        this.peso = peso;
+    }
+
+    public String getDiscVisual() {
+        return discVisual;
+    }
+
+    public void setDiscVisual(String discVisual) {
+        this.discVisual = discVisual;
+    }
+
+    public String getDiabetes() {
+        return diabetes;
+    }
+
+    public void setDiabetes(String diabetes) {
+        this.diabetes = diabetes;
+    }
+
+    public String getHipertención() {
+        return hipertención;
+    }
+
+    public void setHipertención(String hipertención) {
+        this.hipertención = hipertención;
+    }
+    
+   
     
     public String getNumeroSeguro() {
         return numeroSeguro;
